e6e800aa647591dedf7c8164efbd4d30661e9570
Patrick Steele-Idem <pnidem@gmail.com>
2016-04-07 14:25:03 -0600

    Updated publish.sh

--- publish.sh
+++ publish.sh
@@ -5,11 +5,12 @@ rm -rf build/ .cache/
 
 git clone -b gh-pages https://github.com/patrick-steele-idem/dynamically-generated-static-site.git build/
 
-rm -rf build/static/
-
-NODE_ENV=production node build.js
-
 cd build/
+
+git rm -rf .
+
+NODE_ENV=production node ../build.js
+
 git add . --all
 git commit -a -m "Updated website"
 git push origin gh-pages
